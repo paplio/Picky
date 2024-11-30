0df203bdcd2e3c4aaf9ebdef9efc980d8daf081e
paps <ani.12.28.98@gmali.com>
2024-11-30 18:01:15 +0530

    Initial Commit

---
+++ Picky/PickyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickyApp.swift
+//  Picky
+//
+//  Created by Anirudh Bathija on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PickyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
